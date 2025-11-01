424cc450c60fed77b5f30c36a635a035d578692d
pleng <pacharaprompang1@gmail.com>
2025-11-01 21:45:39 +0700

    delete unused component

--- src/utils/validationService.ts
+++
@@ -1,7 +0,0 @@
-function checkFirstName(firstName: string): boolean {
-  return firstName.trim().length >= 2;
-}
-
-function checkLastName(lastName: string): boolean {
-  return lastName.trim().length >= 2;
-}
